fc07340b221e76c2472143beb419fabc2e870df5
ANISH M <58029804+Anish-M-code@users.noreply.github.com>
2020-12-25 15:36:53 +0000

    Add files via upload

---
+++ Python Source Code/Fix/commons.py
@@ -0,0 +1,120 @@
+#flag=imp
+
+''' This is the common library developed to satisfy the needs of  commandline python applications .
+    It contains necessary  std libraries and basic functions for  its users.'''
+	
+	
+try:
+   import hashlib
+   import time
+   import platform
+   import os
+   import getpass
+   import webbrowser
+
+except ImportError:
+    print('Critical Error:Necessary Python Modules are missing!')
+    print('Commons module couldnot satisfy all the dependencies!')
+    x=input('\nPress any key to exit...')
+    exit()
+
+def r(f):
+   if os.path.exists(f):
+      os.remove(f)
+    
+def detect(cmd,web,snam,pkg):
+    
+        if platform.system().lower()=='windows':
+              if os.system(cmd+'>chk')!=0:
+                os.system('cls')
+                print('\nError: '+pkg+' is not detected!')
+                print('\nPlease wait opening '+snam +' website in your browser!\nYou Have to download and install it.\n')
+                webbrowser.open(web)
+                x=input('\nPress any key to exit...')
+                r('chk')
+                exit()
+                
+        else:
+         if os.system(cmd+'>chk')!=0:  
+           os.system('clear')         
+           print('\nError:'+pkg+' is not detected!\n Please install The package to continue.')
+           x=input('\nPress any key to exit...')
+           r('chk')
+           exit()
+        r('chk')
+           
+
+def start():
+    print('\n<-----Task Started----->\n')
+    
+def end():
+    print('\n<-----Task Completed----->\n')
+
+def tsks():
+    start()
+    
+def tske():
+    end()
+
+def tskf():
+    print('\n<-----Task Failed !----->\n')
+
+
+def wait():
+      x=input('\nPress any key to continue...\n')
+
+def display(file):
+        with open(file,'r') as f:
+            s=f.read(1024)
+            print(s)
+            while len(s)>0:
+                s=f.read(1024)
+                print(s)
+    
+
+def gpg():
+   detect('gpg --version','https://gpg4win.org','gpg4win','Gnupg')
+   
+  
+def exiftool():
+   detect('exiftool -h','https://exiftool.org','exiftool','Exiftool')
+
+def ffmpeg():
+   detect('ffmpeg --help','https://ffmpeg.org/','ffmpeg','ffmpeg')
+
+
+def copy(file,file1):
+   if os.path.exists(file):
+    f=open(file,'r')
+    s=open(file1,'a+')
+    if os.path.getsize(file)<(1024*1024*1024):
+      buff=f.read()
+      s.write('\n-------------------------------------------------------\n')
+      s.write(buff)
+    f.close()
+    s.close()
+
+def bcopy(file,file1):
+   if os.path.exists(file):
+    f=open(file,'rb')
+    s=open(file1,'ab+')
+    if os.path.getsize(file)<(1024*1024*1024):
+      buff=f.read()
+      s.write(buff)
+    f.close()
+    s.close()
+
+def cls():
+    if platform.system().lower()=='windows':
+       os.system('cls')
+    else:
+       os.system('clear')
+
+
+
+
+
+
+
+
+    
---
+++ Python Source Code/Fix/emrt.py
@@ -0,0 +1,38 @@
+try:
+  from commons import *
+except ImportError:
+  print('commons Module not Found in emrt!')
+  x=input()
+  exit()
+
+# This is a extension for mrt module 0.1 
+
+def bulkimgScript():
+  ext=input("\nEnter Image's Extension:")
+  if ext[0]!='.':
+      ext='.'+ext
+  if ext not in ('.jpg','.gif','.bmp','.tiff','.jpeg','.png'):
+      print('\nInvalid Filename!')
+      wait()
+      exit()
+  if platform.system().lower()=='windows':
+      with open('runscript.bat','w') as f:
+          f.write('echo off\nexiftool *'+ext+'>input.txt\n')
+          f.write('exiftool -all= *'+ext)
+          f.write('\nexiftool *'+ext+'>output.txt\npause')
+          print('\nScript Generated Successfully!')
+          wait()
+
+  elif platform.system().lower()=='linux':
+      with open('runscript.sh','w') as f:
+          f.write('exiftool *'+ext+'>input.txt\n')
+          f.write('exiftool -all= *'+ext)
+          f.write('\nexiftool *'+ext+'>output.txt\n')
+          print('\nScript Generated Successfully!')
+          wait()
+          
+if __name__=='__main__':
+    print('\nExtension of MRT module\n')
+    print('\nCompatible with all commons module versions!')
+    print('Extension developed by Fixit Project')  
+    wait()
